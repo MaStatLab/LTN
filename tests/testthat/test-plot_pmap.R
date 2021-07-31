1b62c443efc4685395d289a1a28a38f81a0feea6
ZhuoqunWang0120 <zw122@duke.edu>
2021-07-31 17:07:41 -0400

    plot_timepoints

---
+++ tests/testthat/test-plot_pmap.R
@@ -0,0 +1,9 @@
+test_that("consistent sign in plot_pmap()", {
+  for (i in 1:5){
+    pmap=stats::rbeta(9,1,3)
+    tree=ape::rtree(10)
+    alpha=stats::rnorm(9)
+    e=plot_pmap(pmap,tree,'main',alpha=alpha,label_nodes = which(pmap>0.5))
+    expect_identical(e,NULL)
+  }
+})
